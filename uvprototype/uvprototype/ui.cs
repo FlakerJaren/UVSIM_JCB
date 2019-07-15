bcceb124fe8f1e655c0c25a8e0065637453a72a7
Jaren Flaker <jarenflaker@gmail.com>
2019-07-15 16:38:28 -0600

    first commit

---
+++ uvprototype/uvprototype/ui.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvprototype
+{
+    public static class ui
+    {
+        public static void display_instructions()
+        {
+            Console.WriteLine("Welcome to JCB's UV Simulator!\nPlease enter instructions one data word at a time.\nData words are positve or negative 4 digit strings." +
+            "\nWhen you are finished, enter -99999 to stop and run program.\n");
+        }
+
+        public static void get_Input()
+        {
+
+            while (true)
+            {
+                Console.Write("{0:D2} ? ", uvsim.count);
+
+                string user_input = Console.ReadLine();
+                if (user_input == "-99999")
+                    break;
+
+                if (user_input.Length == 5)
+                {
+                    if (user_input[0] == '+' || user_input[0] == '-')
+                    {
+                        //slice string to remove sign in front
+                        uvsim.memory[uvsim.count] = (user_input.Substring(1, 4));
+                        uvsim.count += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Instruction: Missing (+) or (-) Sign");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+
+                
+            }
+        }
+    
+
+    }
+}
---
+++ uvprototype/uvprototype/uvsim.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvprototype
+{
+    public static class uvsim
+    {
+
+        const int MEM_SIZE = 100;
+
+        public static string[] memory = new string[MEM_SIZE];
+        public static int accumilator, pc = 0, operand = 0, opcode = 0, count = 0;
+
+        //I/O Operations
+        public static void read(int op)
+        {
+            Console.Write("Enter an Integer: ");
+            string input = Console.ReadLine();
+            memory[op] = input;
+
+        }
+        public static void write(int op)
+        {
+            Console.WriteLine("Contents of location {0}: {1}", op, memory[op]);
+        }
+
+        //Load/Store Operations
+        public static void load(int op)
+        {
+            accumilator = int.Parse(memory[op]);
+        }
+
+        public static void store(int op)
+        {
+            memory[op] = accumilator.ToString();
+        }
+
+        //ALU
+        public static void add(int op) { accumilator += int.Parse(memory[op]); }
+        public static void subtract(int op) { accumilator -= int.Parse(memory[op]); }
+        public static void divide(int op) { accumilator /= int.Parse(memory[op]); }
+        public static void multiply(int op) { accumilator *= int.Parse(memory[op]); }
+
+        //Control Operations
+        //using op -1 because after the command runs the pc will be incremented by 1
+        public static void branch(int op) { pc = op - 1; }
+        public static void branchNeg(int op) { if (accumilator < 0) { pc = op - 1; } }
+        public static void branchZero(int op) { if (accumilator == 0) { pc = op - 1;} }
+        public static void halt() { pc = 101; }
+
+        //Debugging Operations
+
+
+
+
+        public static void memdump()
+        {
+            Console.WriteLine("\nAccumilator: {0:d2}\nOpcode: {1:d2}\nOperand: {2:d2}\n\nMemory Dump: ", accumilator, opcode, operand);
+            for (int i = 0; i < MEM_SIZE; i++)
+            {
+                Console.WriteLine("{0:d2}: {1}", i, memory[i]);
+            }
+        }
+
+
+        public static void runProgram()
+        {
+            Console.WriteLine("*** Running Program ***");
+            //run code in memory 
+            while (pc < MEM_SIZE)
+            {
+               if(memory[pc] != null)
+                {
+                    opcode = int.Parse(memory[pc].Substring(0, 2));
+                    operand = int.Parse(memory[pc].Substring(2));
+
+                    switch (opcode)
+                    {
+                        case 10:
+                            read(operand);
+                            break;
+                        case 11:
+                            write(operand);
+                            break;
+                        case 20:
+                            load(operand);
+                            break;
+                        case 21:
+                            store(operand);
+                            break;
+                        case 30:
+                            add(operand);
+                            break;
+                        case 31:
+                            subtract(operand);
+                            break;
+                        case 32:
+                            divide(operand);
+                            break;
+                        case 33:
+                            multiply(operand);
+                            break;
+                        case 40:
+                            branch(operand);
+                            break;
+                        case 41:
+                            branchNeg(operand);
+                            break;
+                        case 42:
+                            branchZero(operand);
+                            break;
+                        case 43:
+                            halt();
+                            break;
+                    }
+                }
+
+                //increment program counter
+                pc += 1;
+                
+
+            }
+        }
+    }
+}
+
